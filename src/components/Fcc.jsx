9f9af24c010ebda1180f33e80b533d3e9677a432
Noisycall <rajekunal4@gmail.com>
2019-07-04 22:21:40 +0530

    added some stuff

---
+++ src/components/Fcc.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Fcc extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="">
+          <li className="list-group-item-primary">First</li>
+          <li className="list-group-item-secondary">Second</li>
+          <li className="list-group-item-success">Third</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Fcc;
