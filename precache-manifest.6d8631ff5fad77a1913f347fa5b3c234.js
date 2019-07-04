9edf6276299b0a724ca44c49af60c3908c496465
Noisycall <rajekunal4@gmail.com>
2019-07-04 20:58:12 +0530

    Updates

---
+++ precache-manifest.6d8631ff5fad77a1913f347fa5b3c234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc44d2f3098b5e869bc00fd415089b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb2527d2493145ef893f",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "f6c29f01bff88a29d305",
+    "url": "/static/css/main.344a1d38.chunk.css"
+  },
+  {
+    "revision": "cb2527d2493145ef893f",
+    "url": "/static/js/2.00f73c00.chunk.js"
+  },
+  {
+    "revision": "f6c29f01bff88a29d305",
+    "url": "/static/js/main.cb413c67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
