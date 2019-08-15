321a9232ff72e9b60f9def54570b1ceb62b4e8dd
Noisycall <45820995+Noisycall@users.noreply.github.com>
2019-08-15 15:25:30 +0530

    Removed Old Code

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,24 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Test extends Component {
-    state = {users: []};
+  state = { users: [] };
 
-    componentDidMount(): void {
-        fetch("http://192.168.1.8:3001/users")
-            .then(res => res.json())
-            .then(users => this.setState({users}));
-    }
+  componentDidMount(): void {
+    fetch("http://192.168.1.8:3001/users")
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Users</h1>
-                {this.state.users.map(({id, username}) => (
-                    <div key={id}>{username}</div>
-                ))}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(({ id, username }) => (
+          <div key={id}>{username}</div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Test;
